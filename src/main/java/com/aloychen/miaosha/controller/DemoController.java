8a2520f64b5d4475b895496b3f7520b86c3afa64
aloychenlx <aloychenlx@outlook.com>
2019-03-26 22:19:31 +0800

    add .

--- src/main/java/com/aloychen/miaosha/controller/DemoController.java
+++ src/main/java/com/aloychen/miaosha/controller/DemoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class DemoController {
     //*.idea;*.mvn;*.gitignore;
     //..
+    //、、
     @RequestMapping("/")
     @ResponseBody
     public String home(){
